68cdfc0b2004601f858a3e63d70ff1ae5ed6cc9b
Mokshith-S <mokshithnaikputtur@gmail.com>
2022-02-24 13:47:22 +0530

    Initial

---
+++ client.c
@@ -0,0 +1,29 @@
+#include<fcntl.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+int main()
+{
+char fname[50],buffer[1024];
+int s,n;
+struct sockaddr_in address;
+address.sin_family = AF_INET;
+address.sin_port = htons(15000);
+address.sin_addr.s_addr = INADDR_ANY;
+if((s=socket(AF_INET,SOCK_STREAM,0))<0)
+perror("Socket");
+connect(s,(struct sockaddr*)&address,sizeof(address));
+printf("Enter file name:");
+scanf("%s",fname);
+printf("Sending request\n");
+send(s,fname,sizeof(fname),0);
+while((n=recv(s,buffer,sizeof(buffer),0))>0)
+{
+
+printf("%s\n",buffer);
+printf("Recieved response\n");
+}
+
+return 0;
+}
